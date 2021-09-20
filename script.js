feb20d041a6b93e99701ba9f9afe97bc5a925670
duylan1311 <duylanttvl1398@gmail.com>
2021-09-20 22:09:58 +0700

    updated

---
+++ script.js
@@ -0,0 +1,61 @@
+ready(function() {
+    // Changing the defaults
+    window.sr = ScrollReveal({
+        reset: true
+    });
+
+    // Customizing a reveal set
+    sr.reveal('div', {
+        opacity: 0.9,
+        duration: 3000
+    });
+
+    // Modal Image Gallery
+    function onClick(element) {
+        document.getElementById("img01").src = element.src;
+        document.getElementById("modal01").style.display = "block";
+        var captionText = document.getElementById("caption");
+        captionText.innerHTML = element.alt;
+    }
+
+    // Change style of navbar on scroll
+    window.onscroll = function() {
+        myFunction()
+    };
+
+    function myFunction() {
+        var navbar = document.getElementById("myNavbar");
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        } else {
+            navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        }
+    }
+
+    // Used to toggle the menu on small screens when clicking on the menu button
+    function toggleFunction() {
+        var x = document.getElementById("navDemo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+
+
+    const toTopButton = document.getElementsByClassName('to-top-button')[0];
+    toTopButton.addEventListener('click', () => {
+        window.scrollTo(0, 0);
+    })
+});
+
+function ready(callback) {
+    // in case the document is already rendered
+    if (document.readyState != 'loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function() {
+        if (document.readyState == 'complete') callback();
+    });
+}
